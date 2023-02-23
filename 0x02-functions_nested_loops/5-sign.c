4714f68586b1a206794e2b57ff6a6170daab470e
Onyango Amos <onyangorichard840@gmail.com>
2023-02-22 18:25:08 -0800

    Functions, nested loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_sign - prints the sign of a number
@@ -15,8 +16,8 @@ int print_sign(int n)
 		return (1);
 	} else if (n == 0)
 	{
-		_putchar(' ');
+		printf("%d\n", n);
 		return (0);
 	}
-	return (0);
+		return (0);
 }
